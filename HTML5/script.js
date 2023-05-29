e07f5969166e340c41dd6d9a5518e797b38843af
mepf1987 <maria_figueroa_ed@hotmail.es>
2023-05-29 14:52:54 +0200

    update

--- HTML5/script.js
+++ HTML5/script.js
@@ -1,27 +1,41 @@
-function cargarVideo(event) {
+function loadVideo(file) {
     var videoPlayer = document.getElementById('videoPlayer');
     var loadingMessage = document.getElementById('loadingMessage');
-    var fileInput = document.getElementById('fileInput');
-    var file = event.target.files[0];
-
+  
     // Mostrar el mensaje de carga
     loadingMessage.style.display = 'block';
-
+  
     // Verificar la extensión del video
     if (file.type === 'video/mp4') {
-        var fileReader = new FileReader();
-        fileReader.onload = function () {
-            // Ocultar el mensaje de carga después de que el video se haya cargado
-            loadingMessage.style.display = 'none';
-            videoPlayer.src = fileReader.result;
-            videoPlayer.play();
-        };
-        fileReader.readAsDataURL(file);
-    } else {
-        console.error('Error: El video debe tener formato MP4.');
-        // Ocultar el mensaje de carga en caso de error
+      var fileReader = new FileReader();
+      fileReader.onload = function() {
+        // Ocultar el mensaje de carga después de que el video se haya cargado
         loadingMessage.style.display = 'none';
+        videoPlayer.src = fileReader.result;
+        videoPlayer.play();
+      };
+      fileReader.readAsDataURL(file);
+    } else {
+      console.error('Error: El video debe tener formato MP4.');
+      // Ocultar el mensaje de carga en caso de error
+      loadingMessage.style.display = 'none';
     }
-}
+    
+  }
 
-document.getElementById('fileInput').addEventListener('change', cargarVideo);
+  function manualLoad(event) {
+    event.preventDefault();
+    var fileInput = event.target;
+    var file = fileInput.files[0];
+    loadVideo(file);
+  }
+
+  function handleDrop(event) {
+    event.preventDefault();
+    var file = event.dataTransfer.files[0];
+    loadVideo(file);
+  }
+  
+  function handleDragOver(event) {
+    event.preventDefault();
+  }

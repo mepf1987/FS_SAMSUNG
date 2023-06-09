6a1bde0d8401f94d9c3985c0f43ea3adbfc6ab87
mepf1987 <maria_figueroa_ed@hotmail.es>
2023-06-10 00:02:04 +0200

    update setters

--- TypeScript/Agenda/servicios/pedirDatos.ts
+++ TypeScript/Agenda/servicios/pedirDatos.ts
@@ -8,9 +8,9 @@ export function pedirDNI(persona?: Persona): string {
 
   let dni;
   while (dni == undefined) {
-    persona != null ? console.log("DNI actual:" + persona.getDni()) : ""
+    persona != undefined ? console.log("DNI actual:" + persona.getDni()) : ""
     dni = readlineSync.question("Introduce el DNI:");
-    dni == null ? (persona != null ? dni = persona.getDni() : "") : ""
+    dni == undefined ? (persona != undefined ? dni = persona.getDni() : "") : ""
   }
   return dni;
 }
@@ -21,13 +21,13 @@ export function aniadirOEditarDireccion(persona?: Persona): Direccion[]{
   let accion;
   let numDirecciones = 1;
 
-  if (persona != null) {
+  if (persona != undefined) {
     console.log("Las direcciones registradas son:")
     console.log(persona.getDirecciones());
   }
 
-  persona != null ? accion = readlineSync.question("Acciones a realizar: S - Sobreescribir alguna existente, A - Aniadir a lista actual, E - Empezar nueva lista ") : direcciones = [];
-  persona != null && accion !== 'E' ? direcciones = persona.getDirecciones() : direcciones = [];
+  persona != undefined ? accion = readlineSync.question("Acciones a realizar: S - Sobreescribir alguna existente, A - Aniadir a lista actual, E - Empezar nueva lista ") : direcciones = [];
+  persona != undefined && accion !== 'E' ? direcciones = persona.getDirecciones() : direcciones = [];
   numDirecciones = parseInt(readlineSync.question("Numero de direcciones que quieres aniadir o editar:"))
   
 
@@ -54,53 +54,53 @@ export function aniadirOEditarDireccion(persona?: Persona): Direccion[]{
       let direccionEditar=parseInt(readlineSync.question("Indica el id de la direccion a editar:"));
       console.log("Se va a modificar la direccion con id:" + direcciones[direccionEditar].getId())
 
-      let calle;
+      let calle : string | undefined ;
       while (calle == undefined) {
         console.log("Calle actual:" + direcciones[direccionEditar].getNombreCalle())
         calle = readlineSync.question("Introduce la nueva calle:")
-        calle == null ? (direcciones[direccionEditar].getNombreCalle() != null ? calle = direcciones[direccionEditar].getNombreCalle() : calle = undefined) : direcciones[direccionEditar].setNombreCalle(calle);
+        calle == undefined ? (direcciones[direccionEditar].getNombreCalle() != undefined ? calle = direcciones[direccionEditar].getNombreCalle() : calle = undefined) : direcciones[direccionEditar].setNombreCalle(calle);
       }
 
-      let numero;
+      let numero  : number | undefined;
       while (numero == undefined) {
         console.log("Numero actual:" + direcciones[direccionEditar].getNumero())
         numero = parseInt(readlineSync.question("Introduce el nuevo numero:"))
-        numero == null ? (direcciones[direccionEditar].getNumero() != null ? calle = direcciones[direccionEditar].getNumero() : calle = undefined) : direcciones[direccionEditar].setNumero(numero);
+        numero == undefined ? (direcciones[direccionEditar].getNumero() != undefined? numero = direcciones[direccionEditar].getNumero() : numero = undefined) : direcciones[direccionEditar].setNumero(numero);
       }
 
-      let codigoPostal;
+      let codigoPostal  : string | undefined;
       while (codigoPostal == undefined) {
         console.log("Codigo postal actual:" + direcciones[direccionEditar].getCodigoPostal())
         codigoPostal = readlineSync.question("Introduce el nuevo codigo postal:")
-        codigoPostal == null ? (direcciones[direccionEditar].getCodigoPostal() != null ? calle = direcciones[direccionEditar].getCodigoPostal() : calle = undefined) : direcciones[direccionEditar].setCodigoPostal(codigoPostal);
+        codigoPostal == undefined ? (direcciones[direccionEditar].getCodigoPostal() != undefined ? codigoPostal = direcciones[direccionEditar].getCodigoPostal() : codigoPostal = undefined) : direcciones[direccionEditar].setCodigoPostal(codigoPostal);
       }
 
-      let poblacion;
+      let poblacion  : string | undefined;
       while (poblacion == undefined) {
         console.log("Poblacion actual:" + direcciones[direccionEditar].getPoblacion())
         poblacion = readlineSync.question("Introduce el nuevo poblacion:")
-        poblacion == null ? (direcciones[direccionEditar].getPoblacion() != null ? calle = direcciones[direccionEditar].getPoblacion() : calle = undefined) : direcciones[direccionEditar].setPoblacion(codigoPostal);
+        poblacion == undefined ? (direcciones[direccionEditar].getPoblacion() != undefined ? poblacion = direcciones[direccionEditar].getPoblacion() : poblacion = undefined) : direcciones[direccionEditar].setPoblacion(poblacion);
       }
 
-      let provincia;
+      let provincia  : string | undefined;
       while (provincia == undefined) {
         console.log("Provincia actual:" + direcciones[direccionEditar].getProvincia())
         provincia = readlineSync.question("Introduce el nuevo provincia:")
-        provincia == null ? (direcciones[direccionEditar].getProvincia() != null ? calle = direcciones[direccionEditar].getProvincia() : calle = undefined) : direcciones[direccionEditar].setProvincia(codigoPostal);
+        provincia == undefined ? (direcciones[direccionEditar].getProvincia() != undefined ? provincia = direcciones[direccionEditar].getProvincia() : provincia = undefined) : direcciones[direccionEditar].setProvincia(provincia);
       }
 
       let piso;
       //while(piso==undefined){
       console.log("Piso actual:" + direcciones[direccionEditar].getPiso())
       piso = parseInt(readlineSync.question("Introduce el nuevo piso:"))
-      piso == null ? (direcciones[direccionEditar].getPiso() != null ? calle = direcciones[direccionEditar].getPiso() : calle = undefined) : direcciones[direccionEditar].setPiso(piso);
+      piso == undefined ? (direcciones[direccionEditar].getPiso() != undefined ? piso = direcciones[direccionEditar].getPiso() : piso = undefined) : direcciones[direccionEditar].setPiso(piso);
       //}
 
-      let letra;
+      let letra : string | undefined;
       //while(letra==undefined){
       console.log("Letra actual:" + direcciones[direccionEditar].getLetra())
       letra = readlineSync.question("Introduce el nuevo letra:")
-      letra == null ? (direcciones[direccionEditar].getLetra() != null ? calle = direcciones[direccionEditar].getLetra() : calle = undefined) : direcciones[direccionEditar].setLetra(letra);
+      letra == undefined ? (direcciones[direccionEditar].getLetra() != undefined ? letra = direcciones[direccionEditar].getLetra() : letra = undefined) : direcciones[direccionEditar].setLetra(letra);
       //}
 
       numDirecciones -= 1
@@ -118,25 +118,25 @@ export function pedirDatosPersona(persona?: Persona): Persona {
   //Nombre
   let nombre  : string | undefined;
   while (nombre == undefined) {
-    persona != null ? console.log("Nombre actual:" + persona.getNombre()) : ""
+    persona != undefined ? console.log("Nombre actual:" + persona.getNombre()) : ""
     nombre = readlineSync.question("Introduce el nombre:");
-    nombre == null ? (persona != null ? nombre = persona.getNombre() : "") : ""
+    nombre == undefined ? (persona != undefined ? nombre = persona.getNombre() : "") : ""
   }
 
   //Apellidos
   let apellidos : string | undefined;
   while (apellidos == undefined) {
-    persona != null ? console.log("Apellidos actuales:" + persona.getApellidos()) : ""
+    persona != undefined ? console.log("Apellidos actuales:" + persona.getApellidos()) : ""
     apellidos = readlineSync.question("Introduce los apellidos:");
-    apellidos == null ? (persona != null ? apellidos = persona.getApellidos() : "") : ""
+    apellidos == undefined ? (persona != undefined ? apellidos = persona.getApellidos() : "") : ""
   }
 
   //Edad
   let edad : number | undefined;
   while (edad == undefined) {
-    persona != null ? console.log("Edad actual:" + persona.getEdad()) : ""
+    persona != undefined ? console.log("Edad actual:" + persona.getEdad()) : ""
     edad = parseInt(readlineSync.question("Introduce la edad:"));
-    edad == null ? (persona != null ? edad = persona.getEdad() : "") : ""
+    edad == undefined ? (persona != undefined ? edad = persona.getEdad() : "") : ""
   }
 
   //DNI
@@ -145,26 +145,26 @@ export function pedirDatosPersona(persona?: Persona): Persona {
   //CUMPLEANIOS
   let cumpleanios: Date | undefined;
   while (cumpleanios == undefined) {
-    persona != null ? console.log("Cumpleanios actual:" + persona.getCumpleanios()) : ""
+    persona != undefined ? console.log("Cumpleanios actual:" + persona.getCumpleanios()) : ""
     cumpleanios = new Date(readlineSync.question("Introduce la fecha de cumpleanios con el siguiente formato 'yyyy-mm-dd':"))
-    cumpleanios == null ? (persona != null ? cumpleanios = persona.getCumpleanios() : "") : ""
+    cumpleanios == undefined ? (persona != undefined ? cumpleanios = persona.getCumpleanios() : "") : ""
   }
 
 
   //COLOR FAVORITO
   let colorFavorito : string | undefined;
   while (colorFavorito == undefined) {
-    persona != null ? console.log("ColorFavorito actual:" + persona.getColorFavorito()) : ""
+    persona != undefined ? console.log("ColorFavorito actual:" + persona.getColorFavorito()) : ""
     colorFavorito = readlineSync.question("Introduce el color favorito:")
-    colorFavorito == null ? (persona != null ? colorFavorito = persona.getColorFavorito() : "") : ""
+    colorFavorito == undefined ? (persona != undefined ? colorFavorito = persona.getColorFavorito() : "") : ""
   }
 
   //SEXO
   let sexo : string | undefined;
   while (sexo === undefined) {
-    persona != null ? console.log("Sexo actual:" + persona.getSexo()) : ""
+    persona != undefined ? console.log("Sexo actual:" + persona.getSexo()) : ""
     sexo = readlineSync.question("Introduce el sexo (H:Hombre | M:Mujer):")
-    sexo == null ? (persona != null ? sexo = persona.getSexo() : "") : (sexo === "H" || sexo === "M") ? "" : sexo = undefined;
+    sexo == undefined ? (persona != undefined ? sexo = persona.getSexo() : "") : (sexo === "H" || sexo === "M") ? "" : sexo = undefined;
   }
 
   //DIRECCION
